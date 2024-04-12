b11919a9eb588a19e8ccfb04ad599662cc579240
Keith Gabryelski <keith@gabryelski.com>
2024-04-12 00:49:12 -0400

    add name and group dropdown

--- forsaken-app/src/damage-min-max/index.js
+++ forsaken-app/src/damage-min-max/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Chart } from "primereact/chart";
+import { Dropdown } from "primereact/dropdown";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import DungeonsOfEternityCache from "../models/DungeonsOfEternityCache";
 
-export default function StackedBarDemo() {
+export default function DamageMINMAX() {
   const [cache, setCache] = useState(new DungeonsOfEternityCache());
   const [loading, setLoading] = useState(true);
 
@@ -24,100 +25,124 @@ export default function StackedBarDemo() {
     fetcher();
   }, []);
 
+  const dataSources = [
+    { name: "Group", code: "group" },
+    { name: "Name", code: "name" },
+  ];
+
+  const [chartDataSource, setChartDataSource] = useState("group");
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
 
   useEffect(() => {
-    const ds = [...cache.indexes.byGroup.entries()].reduce(
-      (damages, entry) => {
-        const [groupName, drops] = entry;
-        for (const drop of drops) {
-          const { Rarity, Damage } = drop;
-          if (Damage) {
-            const minmax = damages[Rarity][groupName];
-            if (minmax[0] == null) {
-              damages[Rarity][groupName] = [Damage, Damage];
-            } else {
-              const [min, max] = minmax;
-              damages[Rarity][groupName] = [
-                Math.min(min, Damage),
-                Math.max(max, Damage),
-              ];
-            }
+    if (loading) {
+      return;
+    }
+    const groupNames = [...cache.indexes.byGroup.keys()].filter(
+      (n) => n !== "shields",
+    );
+    const names = [...cache.indexes.byName.entries()]
+      .filter(
+        ([n, drops]) =>
+          drops[0]?.Group !== "shields" &&
+          !["ice staff", "healing staff", "shockwave staff"].includes(n),
+      )
+      .map((e) => e[0]);
+    console.info("groupNames", groupNames);
+    console.info("names", names);
+
+    const chartDataSourceOptions = {
+      group: {
+        index: cache.indexes.byGroup,
+        initializer: {
+          rare: Object.fromEntries(groupNames.map((n) => [n, [null, null]])),
+          legendary: Object.fromEntries(
+            groupNames.map((n) => [n, [null, null]]),
+          ),
+        },
+      },
+      name: {
+        index: cache.indexes.byName,
+        initializer: {
+          rare: Object.fromEntries(names.map((n) => [n, [null, null]])),
+          legendary: Object.fromEntries(names.map((n) => [n, [null, null]])),
+        },
+      },
+    };
+
+    const { index, initializer } = chartDataSourceOptions[chartDataSource];
+
+    let i = 0;
+    const ds = [...index.entries()].reduce((damages, entry) => {
+      const [groupName, drops] = entry;
+      for (const drop of drops) {
+        const { Rarity, Damage } = drop;
+        if (Damage) {
+          const minmax = damages[Rarity][groupName];
+          if (minmax == null) {
+            console.info("not found", groupName);
+          } else if (minmax[0] == null) {
+            damages[Rarity][groupName] = [Damage, Damage];
+          } else {
+            const [min, max] = minmax;
+            damages[Rarity][groupName] = [
+              Math.min(min, Damage),
+              Math.max(max, Damage),
+            ];
           }
         }
+      }
 
-        return damages;
-      },
-      {
-        rare: {
-          staves: [null, null],
-          daggers: [null, null],
-          axes: [null, null],
-          bows: [null, null],
-          crossbows: [null, null],
-          swords: [null, null],
-          hammers: [null, null],
-        },
-        legendary: {
-          staves: [null, null],
-          daggers: [null, null],
-          axes: [null, null],
-          bows: [null, null],
-          crossbows: [null, null],
-          swords: [null, null],
-          hammers: [null, null],
-        },
-      },
-    );
+      console.info("***", i++, JSON.stringify(damages, undefined, 2));
+      return damages;
+    }, initializer);
 
     const documentStyle = getComputedStyle(document.documentElement);
     const textColorSecondary = documentStyle.getPropertyValue(
       "--text-color-secondary",
     );
     const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
-    const baseLabels = [...Object.keys(ds.rare)];
-    const labels = baseLabels
-      .map((g) => [`rare ${g}`, `legendary ${g}`])
-      .flat();
-
-    const datamax = baseLabels
-      .map((g) => [
-        [ds.rare[g][0], ds.rare[g][1]],
-        [ds.legendary[g][0], ds.legendary[g][1]],
-      ])
-      .flat();
+    const labels = [...Object.keys(ds.rare)];
 
     const data = {
       labels,
       datasets: [
         {
-          type: "bar",
-          label: "Min/Max",
-          backgroundColor: [
-            "#333300",
-            "#555500",
-            "#006600",
-            "#009900",
-            "#003333",
-            "#005555",
-            "#000066",
-            "#000099",
-            "#330033",
-            "#550055",
-            "#660000",
-            "#990000",
-            "#ff6600",
-            "#ff8800",
-          ],
-          data: datamax,
+          label: "rare",
+          stack: "Stack 0",
+          backgroundColor: ["#0000aa"],
+          data: labels
+            .map((g) => [ds.rare[g][0], ds.rare[g][1]])
+            .map((l, i) =>
+              l[0] == null
+                ? [ds.legendary[labels[i]][0], ds.legendary[labels[i]][1]]
+                : l,
+            ),
+        },
+        {
+          label: "legendary",
+          stack: "Stack 1",
+          backgroundColor: ["#550055"],
+          data: labels
+            .map((g) => [ds.legendary[g][0], ds.legendary[g][1]])
+            .map((l, i) =>
+              l[0] == null ? [ds.rare[labels[i]][0], ds.rare[labels[i]][1]] : l,
+            ),
         },
       ],
     };
     const options = {
       maintainAspectRatio: false,
       aspectRatio: 0.8,
+      responsive: true,
+      interaction: {
+        intersect: false,
+      },
       plugins: {
+        title: {
+          display: true,
+          text: "Weapon's Damage MIN/MAX",
+        },
         datalabels: {
           anchor: "start", // Anchor the labels to the start of the datapoint
           align: "end", // Align the text after the anchor point
@@ -138,7 +163,7 @@ export default function StackedBarDemo() {
           },
         },
         legend: {
-          display: false,
+          display: true,
         },
       },
       indexAxis: "x",
@@ -166,19 +191,31 @@ export default function StackedBarDemo() {
 
     setChartData(data);
     setChartOptions(options);
-  }, [cache.indexes.byGroup]);
+  }, [chartDataSource, cache.indexes.byGroup, cache.indexes.byName, loading]);
 
   if (loading) {
     return null;
   }
   return (
-    <div className="card">
+    <div>
+      <div className="card flex justify-content-center">
+        <Dropdown
+          value={chartDataSource}
+          onChange={(e) => setChartDataSource(e.value.code)}
+          options={dataSources}
+          optionLabel="name"
+          placeholder="Select a Datasource"
+          className="w-full md:w-14rem"
+        />
+      </div>
       <Chart
         plugins={[ChartDataLabels]}
         type="bar"
         data={chartData}
         options={chartOptions}
       />
+      <h3>Notes:</h3>
+      <p>Click on the legend to hide datasets</p>
     </div>
   );
 }
