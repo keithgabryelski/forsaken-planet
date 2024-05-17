ec94dec0a1d70984dcc68f1754753a53d324c0fb
Keith Gabryelski <keith@gabryelski.com>
2024-05-17 08:40:54 -0400

    clean up colors for grouped min/max

--- website/src/app/damage-min-max-grouped/renderer.tsx
+++ website/src/app/damage-min-max-grouped/renderer.tsx
@@ -112,8 +112,9 @@ export default function Renderer({ reports }: { reports: DOEReport[] }) {
       "#194e31",
       "#5a1ba9",
       "#3a49da",
+      "#91db8b",
+      "#ad7bee",
       "#1af9ff",
-      "#6d003d",
     ];
 
     const tooltips = [];
