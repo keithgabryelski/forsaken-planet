0da7b2e8319cb92558ce3cae68cc9049db30fa67
Keith Gabryelski <kgabryelski@salesforce.com>
2025-05-11 10:29:22 -0400

    remove cruft

--- install/install3.sh
+++
@@ -1,9 +0,0 @@
-# https://docs.aws.amazon.com/linux/al2023/ug/SSL-on-amazon-linux-2023.html
-echo adding root CERTS
-
-dnf install openssl mod_ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/pki/tls/private/apache-selfsigned.key -out /etc/pki/tls/certs/apache-selfsigned.crt
-systemctl restart httpd
-
-echo "you should get stuff signed by an authority"
-echo "go to install4.sh"
